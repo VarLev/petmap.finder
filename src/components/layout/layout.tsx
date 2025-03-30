0aba24597aaf4308d9369e15a893cd0cc7d9c0f7
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-03-30 19:53:51 +0300

    add icons to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,7 @@
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { Dispatch, FC, ReactNode, SetStateAction } from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import MapIcon from '@mui/icons-material/Map';
 
 type LayoutProps = {
     children: ReactNode;
@@ -8,8 +10,9 @@ type LayoutProps = {
 }
 
 const Layout: FC<LayoutProps> = ({children, pageType, setPageType}) => {
+  
   return (
-    <div className='bg-[white] w-[400px] rounded-[20px] h-[100%]'>
+    <div className='bg-[white] w-[calc(100%-200px)] mx-auto rounded-[20px] h-[100%] min-w-[600px]'>
         {children}
         <BottomNavigation
             showLabels
@@ -17,8 +20,8 @@ const Layout: FC<LayoutProps> = ({children, pageType, setPageType}) => {
             onChange={(_, newValue) => setPageType(newValue)}
             className='rounded-[20px]'
         >
-            <BottomNavigationAction value='form' icon={<>Форма</>} />
-            <BottomNavigationAction value='map' icon={<>Карта</>} />
+            <BottomNavigationAction value='form' icon={<EditNoteIcon fontSize='large' sx={{color: pageType === 'form' ? "#2F00B6" : 'grey'}}/>} />
+            <BottomNavigationAction value='map' icon={<MapIcon fontSize='large' sx={{color: pageType === 'map' ? "#2F00B6" : 'grey'}}/>} />
         </BottomNavigation>
     </div>
   )
